16f96eacdf4958a602942fc2da020cce40e9a7fb
Muhammet Cin <muhammetacin@gmail.com>
2022-02-01 11:15:49 +0100

    Clean comments

--- Part2-JavaScript-Modules/js/index.js
+++ Part2-JavaScript-Modules/js/index.js
@@ -5,16 +5,6 @@ import { cityNameInput, cityImage, showCityName, todaysDate, cityProperties, day
 import fetchCityTemperature5Days from './fetch-temperatures.js';
 import fetchCityImage from './fetch-image.js';
 
-// const cityNameInput = document.getElementById('inputField');
-// const submitBtn = document.getElementById('submit');
-// const cityProperties = document.getElementById('cityProperties');
-// const dayNamesOfWeek = document.getElementById('daysOfWeek');
-// const showCityName = document.getElementById('inputArea').children[0];
-// const cityImage = document.getElementById('cityImg');
-
-// // Get todays day
-// const todaysDate = new Date().getDay();
-
 const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
 };
@@ -32,48 +22,6 @@ const resetCityValues = () => {
     cityImage.src = "";
 };
 
-// const setDayNames = (today) => {
-//     let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-//     // Rearrange the array
-//     while(today != 0) {
-//         let day = daysOfWeek.shift();
-//         daysOfWeek.push(day);
-//         today--;
-//     }
-
-//     // Show correct day names on page
-//     for(let i = 0; i < dayNamesOfWeek.children.length; i++) {
-//         dayNamesOfWeek.children[i].children[0].innerHTML = daysOfWeek[i + 1];
-//     }
-
-//     return daysOfWeek;
-// };
-
-// const setTemperatureOfDays = (temperatureData) => {
-//     let temperatures = [];
-    
-//     const todaysTemperature = Math.round(temperatureData[0]);
-//     temperatures.push(todaysTemperature);
-
-//     cityProperties.children[2].innerHTML = todaysTemperature + "°";
-    
-//     for(let i = 0; i < dayNamesOfWeek.children.length; i++) {
-//         dayNamesOfWeek.children[i].children[2].innerHTML = Math.round(temperatureData[i + 1]) + "°";
-//         temperatures.push(Math.round(temperatureData[i + 1]));
-//     }
-
-//     return temperatures;
-// };
-
-// const setWeatherDescriptionOfDays = (weatherDescription) => {
-//     cityProperties.children[1].innerHTML = weatherDescription[0];
-
-//     for(let i = 0; i < dayNamesOfWeek.children.length; i++) {
-//         dayNamesOfWeek.children[i].children[1].innerHTML = weatherDescription[i + 1];
-//     }
-// };
-
 const setCityProperties = (responseCityName, responseTemperatureValues, weatherDescription) => {
     // Set all the weekdays in app correctly according to day
     setDayNames(todaysDate);
@@ -87,14 +35,6 @@ const setCityProperties = (responseCityName, responseTemperatureValues, weatherD
     // Show sky description for all days
     setWeatherDescriptionOfDays(weatherDescription);
 };
-
-// const createGraphWithValues = (responseTemperatureValues) => {
-//     const temperatures = setTemperatureOfDays(responseTemperatureValues);
-//     const dayLabels = setDayNames(todaysDate);
-//     dayLabels.pop();
-
-//     drawGraph(dayLabels, temperatures);
-// };
 
 const executeInput = () => {
     let inputValue = capitalizeFirstLetter(cityNameInput.value);
